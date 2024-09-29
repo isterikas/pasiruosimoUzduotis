847d75d93d91fd9370c0deea6f5d6b3b84551ca6
Eryk Tatol <eryk.tatol@stud.techin.lt>
2024-09-29 21:04:18 +0300

    cleanup

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("submitButton")
-function message(e) {
-    e.preventDefault()
-    window.confirm("Form submission successful!");    
-}
-button.addEventListener("click", message);
